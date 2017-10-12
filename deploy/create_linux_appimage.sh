c3a8ed043193afab887759da9d0fc9e71d7bd7e1
Kurt Kiefer <kekiefer@gmail.com>
2017-10-11 21:38:30 -0700

    Use linuxdeployqt for linux appimage builds

--- deploy/create_linux_appimage.sh
+++ deploy/create_linux_appimage.sh
@@ -39,19 +39,19 @@ cd ${APPDIR}
 find ../ -name *.deb -exec dpkg -x {} . \;
 
 # copy libdirectfb-1.2.so.9
-cd ${TMPDIR}
-wget -c --quiet http://ftp.us.debian.org/debian/pool/main/d/directfb/libdirectfb-1.2-9_1.2.10.0-5.1_amd64.deb
-mkdir libdirectfb
-dpkg -x libdirectfb-1.2-9_1.2.10.0-5.1_amd64.deb libdirectfb
-cp -L libdirectfb/usr/lib/x86_64-linux-gnu/libdirectfb-1.2.so.9 ${APPDIR}/usr/lib/x86_64-linux-gnu/
-cp -L libdirectfb/usr/lib/x86_64-linux-gnu/libfusion-1.2.so.9 ${APPDIR}/usr/lib/x86_64-linux-gnu/
-cp -L libdirectfb/usr/lib/x86_64-linux-gnu/libdirect-1.2.so.9 ${APPDIR}/usr/lib/x86_64-linux-gnu/
-
-# copy libts-0.0-0
-wget -c --quiet http://ftp.us.debian.org/debian/pool/main/t/tslib/libts-0.0-0_1.0-11_amd64.deb
-mkdir libts
-dpkg -x libts-0.0-0_1.0-11_amd64.deb libts
-cp -L libts/usr/lib/x86_64-linux-gnu/libts-0.0.so.0 ${APPDIR}/usr/lib/x86_64-linux-gnu/
+#cd ${TMPDIR}
+#wget -c --quiet http://ftp.us.debian.org/debian/pool/main/d/directfb/libdirectfb-1.2-9_1.2.10.0-5.1_amd64.deb
+#mkdir libdirectfb
+#dpkg -x libdirectfb-1.2-9_1.2.10.0-5.1_amd64.deb libdirectfb
+#cp -L libdirectfb/usr/lib/x86_64-linux-gnu/libdirectfb-1.2.so.9 ${APPDIR}/usr/lib/x86_64-linux-gnu/
+#cp -L libdirectfb/usr/lib/x86_64-linux-gnu/libfusion-1.2.so.9 ${APPDIR}/usr/lib/x86_64-linux-gnu/
+#cp -L libdirectfb/usr/lib/x86_64-linux-gnu/libdirect-1.2.so.9 ${APPDIR}/usr/lib/x86_64-linux-gnu/
+#
+## copy libts-0.0-0
+#wget -c --quiet http://ftp.us.debian.org/debian/pool/main/t/tslib/libts-0.0-0_1.0-11_amd64.deb
+#mkdir libts
+#dpkg -x libts-0.0-0_1.0-11_amd64.deb libts
+#cp -L libts/usr/lib/x86_64-linux-gnu/libts-0.0.so.0 ${APPDIR}/usr/lib/x86_64-linux-gnu/
 
 # copy GetThermal release into appimage
 cp -r ${GT_RELEASE_DIR}/* ${APPDIR}/
@@ -61,6 +61,9 @@ mv ${APPDIR}/getthermal-start.sh ${APPDIR}/AppRun
 # copy icon
 cp ${GT_SRC}/icons/getthermal.png ${APPDIR}/
 
+# copy desktop file
+cp ${GT_SRC}/deploy/getthermal.desktop ${APPDIR}/
+
 #cat > ./getthermal.desktop <<\EOF
 #[Desktop Entry]
 #Type=Application
@@ -68,21 +71,25 @@ cp ${GT_SRC}/icons/getthermal.png ${APPDIR}/
 #GenericName=GetThermal
 #Comment=Cross-platform USB thermal camera viewer
 #Icon=getthermal
-#Exec=AppRun
+#Exec=GetThermal
 #Terminal=false
 #Categories=Utility;
 #Keywords=computer;
 #EOF
 
-VERSION=$(strings ${APPDIR}/getthermal | grep '^v[0-9*]\.[0-9*].[0-9*]' | head -n 1)
+#VERSION=$(strings ${APPDIR}/GetThermal | grep '^v[0-9*]\.[0-9*].[0-9*]' | head -n 1)
+pushd $GT_SRC
+VERSION=$(git describe --always --tags)
+popd
 echo GT Version: ${VERSION}
 
 # Go out of AppImage
 cd ${TMPDIR}
-wget -c --quiet "https://github.com/probonopd/AppImageKit/releases/download/5/AppImageAssistant" # (64-bit)
-chmod a+x ./AppImageAssistant
 
-./AppImageAssistant ./$APP.AppDir/ ${TMPDIR}/$APP".AppImage"
+wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt*.AppImage
 
-cp ${TMPDIR}/$APP".AppImage" ${OUTPUT_DIR}/$APP".AppImage"
+./linuxdeployqt*.AppImage ./$APP.AppDir/*.desktop -bundle-non-qt-libs
+./linuxdeployqt*.AppImage ./$APP.AppDir/*.desktop -qmldir=${GT_SRC}/qml -appimage
 
+cp ${TMPDIR}/${APP}*".AppImage" ${OUTPUT_DIR}/
